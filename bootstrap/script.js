4607b4487631080a63bc6edc541e707a1c5f835d
theresenn <therese.nielsen0@gmail.com>
2017-03-09 12:59:40 +0100

    getMovieID

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -1,10 +1,23 @@
-function getRandomID() {
-    var movieID = Math.floor((Math.random() * 1199999) + 0000001);
-    console.log(movieID);
-    return movieID;
+function getRandomID(number, length) {
+  var str = '' + number;
+  while(str.length < length) {
+    str = '0' + str;
+  }
+  return str;
 }
-    
 getRandomID();
 
+var movieID = getRandomID(Math.floor((Math.random() * 2155529) + 1), 7);
+console.log(movieID);
 
 
+function getMovie () {
+    $("#resultOmdb").show();
+    $.ajax({
+        url: "http://www.omdbapi.com/?i=tt" + movieID + "&y=&plot=short&r=json",
+        dataType: "JSON",
+        success: function(data){
+            console.log(data);
+        }
+    })
+}
