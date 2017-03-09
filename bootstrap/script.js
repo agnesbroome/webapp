08db78a79bd42a4a1283d4d0a60e5da0ba17ad1a
theresenn <therese.nielsen0@gmail.com>
2017-03-09 13:46:43 +0100

    showMovieID

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -10,7 +10,7 @@ getRandomID();
 
 
 
-$("#random").on("click", function getMovie () {
+$("#random").on("click", function getMovie() {
     var movieID = getRandomID(Math.floor((Math.random() * 2155529) + 1), 7);
 console.log(movieID);
     $("#resultOmdb").show();
@@ -18,8 +18,11 @@ console.log(movieID);
         url: "http://www.omdbapi.com/?i=tt" + movieID + "&y=&plot=short&r=json",
         dataType: "JSON",
         success: function(data){
+            $("#resultOmdb").append("<h4>" + movieID + "</h4>");
             console.log(data);
         }
     })
+    getMovie();
 });
-getMovie ();
+
+
