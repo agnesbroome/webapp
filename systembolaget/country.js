cd2f24807e8933d716061798678bf88f1094d2ee
mariaholmberg283 <mariaholmberg283@hotmail.com>
2017-03-10 14:53:58 +0100

    lista

---
+++ systembolaget/country.js
@@ -0,0 +1,406 @@
+[
+  {
+    "eng": "France",
+    "id": "1",
+    "sv": "Frankrike"
+  },
+  {
+    "eng": "Sweden",
+    "id": "2",
+    "sv": "Sverige"
+  },
+  {
+    "eng": "Poland",
+    "id": "3",
+    "sv": "Polen"
+  },
+  {
+    "eng": "United States of America",
+    "id": "4",
+    "sv": "USA"
+  },
+  {
+    "id": "5",
+    "name": "Storbritannien"
+  },
+  {
+    "id": "6",
+    "name": "Belgien"
+  },
+{
+    "id": "7",
+    "name": "Nederländerna"
+  },
+  {
+    "id": "8",
+    "name": "Trinidad"
+  },
+  {
+    "id": "9",
+    "name": "Schweiz"
+  },
+  {
+    "id": "10",
+    "name": "Internationellt märke"
+  },
+  {
+    "id": "11",
+    "name": "Italien"
+  },
+  {
+    "id": "12",
+    "name": "Mexiko"
+  },
+  {
+    "id": "13",
+    "name": "Danmark"
+  },
+{
+    "id": "14",
+    "name": "Chile"
+  },
+  {
+    "id": "15",
+    "name": "Spanien"
+  },
+  {
+    "id": "16",
+    "name": "Österrike"
+  },
+  {
+    "id": "17",
+    "name": "Barbados"
+  },
+  {
+    "id": "18",
+    "name": "Norge"
+  },
+  {
+    "id": "19",
+    "name": "Japan"
+  },
+  {
+    "id": "20",
+    "name": "Förpackning"
+  },
+{
+    "id": "21",
+    "name": "Irland"
+  },
+  {
+    "id": "22",
+    "name": "Peru"
+  },
+  {
+    "id": "23",
+    "name": "Finland"
+  },
+  {
+    "id": "24",
+    "name": "Tyskland"
+  },
+  {
+    "id": "25",
+    "name": "Australien"
+  },
+  {
+    "id": "26",
+    "name": "Kanada"
+  },
+  {
+    "id": "27",
+    "name": "Sydafrika"
+  },
+{
+    "id": "28",
+    "name": "Tjeckien"
+  },
+  {
+    "id": "29",
+    "name": "Nya Zeeland"
+  },
+  {
+    "id": "30",
+    "name": "Ungern"
+  },
+  {
+    "id": "31",
+    "name": "Israel"
+  },
+  {
+    "id": "32",
+    "name": "Portugal"
+  },
+  {
+    "id": "33",
+    "name": "Colombia"
+  },
+  {
+    "id": "34",
+    "name": "Grekland"
+  },
+{
+    "id": "34",
+    "name": "Grekland"
+  },
+  {
+    "id": "35",
+    "name": "Thailand"
+  },
+  {
+    "id": "36",
+    "name": "Kroatien"
+  },
+  {
+    "id": "37",
+    "name": "Turkiet"
+  },
+  {
+    "id": "38",
+    "name": "Jamaica"
+  },
+  {
+    "id": "39",
+    "name": "Ryssland"
+  },
+  {
+    "id": "40",
+    "name": "Argentina"
+  },
+{
+    "id": "41",
+    "name": "Slovakien"
+  },
+  {
+    "id": "42",
+    "name": "Cypern"
+  },
+  {
+    "id": "43",
+    "name": "Övriga ursprung"
+  },
+  {
+    "id": "44",
+    "name": "Bulgarien"
+  },
+  {
+    "id": "45",
+    "name": "Rumänien"
+  },
+  {
+    "id": "46",
+    "name": "Kuba"
+  },
+  {
+    "id": "47",
+    "name": "Haiti"
+  },
+{
+    "id": "48",
+    "name": "Venezuela"
+  },
+  {
+    "id": "49",
+    "name": "Dominikanska Republiken"
+  },
+  {
+    "id": "50",
+    "name": "Indien"
+  },
+  {
+    "id": "51",
+    "name": "Guatemala"
+  },
+  {
+    "id": "52",
+    "name": "Indonesien"
+  },
+  {
+    "id": "53",
+    "name": "Panama"
+  },
+  {
+    "id": "54",
+    "name": "Paraguay"
+  },
+{
+    "id": "55",
+    "name": "Guyana"
+  },
+  {
+    "id": "56",
+    "name": "Varierande ursprung"
+  },
+  {
+    "id": "57",
+    "name": "Serbien"
+  },
+  {
+    "id": "58",
+    "name": "Brasilien"
+  },
+  {
+    "id": "59",
+    "name": "Georgien"
+  },
+  {
+    "id": "60",
+    "name": "Libanon"
+  },
+  {
+    "id": "61",
+    "name": "Bahamas"
+  },  
+{
+    "id": "62",
+    "name": "Slovenien"
+  },
+  {
+    "id": "63",
+    "name": "Okänt ursprung"
+  },
+  {
+    "id": "64",
+    "name": "Namibia"
+  },
+  {
+    "id": "65",
+    "name": "Makedonien (F.Y.R.O.M)"
+  },
+  {
+    "id": "66",
+    "name": "Tunisien"
+  },
+  { 
+    "id": "67",
+    "name": "Uruguay"
+  },
+  {
+    "id": "68",
+    "name": "Montenegro"
+  },
+  {
+    "id": "69",
+    "name": "Sydkorea"
+  },
+  {
+    "id": "70",
+    "name": "Bosnien-Hercegovina"
+  },
+  {
+    "id": "71",
+    "name": "Luxemburg"
+  },
+  {
+    "id": "72",
+    "name": "Malta"
+  },
+  {
+    "id": "73",
+    "name": "Armenien"
+  },  
+{
+    "id": "74",
+    "name": "Moldavien"
+  },
+  {
+    "id": "75",
+    "name": "Lettland"
+  },
+  {
+    "id": "76",
+    "name": "St Lucia"
+  },
+  {
+    "id": "77",
+    "name": "Ukraina"
+  },
+  {
+    "id": "78",
+    "name": "Island"
+  },
+  {
+    "id": "79",
+    "name": "Grenada"
+  },
+  {
+    "id": "80",
+    "name": "Filippinerna"
+  },
+  {
+    "id": "81",
+    "name": "Surinam"
+  },
+  {
+    "id": "82",
+    "name": "Litauen"
+  },
+  {
+    "id": "83",
+    "name": "Fiji"
+  },
+  {
+    "id": "84",
+    "name": "Nicaragua"
+  },
+  {
+    "id": "85",
+    "name": "Estland"
+  },
+  {
+    "id": "86",
+    "name": "Mauritius"
+  },
+  {
+    "id": "87",
+    "name": "Costa Rica"
+  },  
+{
+    "id": "88",
+    "name": "Kosovo"
+  },
+  {
+    "id": "89",
+    "name": "Singapore"
+  },
+  {
+    "id": "90",
+    "name": "Marocko"
+  },
+  {
+    "id": "91",
+    "name": "Kenya"
+  },
+  {
+    "id": "92",
+    "name": "Uganda"
+  },
+  {
+    "id": "93",
+    "name": "Vietnam"
+  },  
+{
+    "id": "94",
+    "name": "Nigeria"
+  },
+  {
+    "id": "95",
+    "name": "Folkrepubliken Kina"
+  },
+  {
+    "id": "96",
+    "name": "Etiopien"
+  },
+  {
+    "id": "97",
+    "name": "Sri Lanka"
+  },
+  {
+    "id": "98",
+    "name": "Republiken Togo"
+  },
+  {
+    "id": "99",
+    "name": "Tanzania"
+  }
+]
