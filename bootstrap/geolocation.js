ab060b7f1c76570318726e368b3c3bff5d80e26b
Agnes Broomé Clason <agnesbroomeclason@agnes-air.clients.internal.mah.se>
2017-03-10 13:27:24 +0100

    Update css and info

--- bootstrap/geolocation.js
+++ bootstrap/geolocation.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 function getGroceries(position){
 
   var currentLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
-
+  
   map = new google.maps.Map(document.getElementById('map'), {
     center: currentLocation,
     zoom: 14
