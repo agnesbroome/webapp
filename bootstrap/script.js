3ee7d83973c36bb72008eea2e4716ca9c88fc9d2
mariaholmberg283 <mariaholmberg283@hotmail.com>
2017-03-13 13:20:04 +0100

    change js

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -36,16 +36,13 @@ $("#random").on("click", function getMovie() {
                 var date = new Date (data.release_date);
                 $("#resultOmdb").append('<div id="movieDetails"></div>');
                 if (data.title != data.original_title){
-                    $("#movieDetails").append("<h3>" + data.title + "</h3>" + "<h3 id='originalTitle'> <small>" +  data.original_title + " </h3> </small>" + "<h4>" + date.getFullYear() + "</h4>" + "<h4>" + data.production_countries[0]["name"] + "</h4" + "<p>" + data.overview + "</p>");}
+                    $("#movieDetails").append("<h3>" + data.title + "</h3>" + "<h3 id='originalTitle'> <small>" +  data.original_title + " </h3> </small>" + "<h4>" + date.getFullYear() + "</h4>" + "<h4>" + data.production_countries[0]["name"] + "</h4>" + "<p>" + data.overview + "</p>");}
                 else {
                     $("#movieDetails").append("<h3>" + data.original_title + "</h3>" + "<h4>" + date.getFullYear() + "</h4>" + "<h4>" + data.production_countries[0]["name"] + "</h4" + "<p>" + data.overview + "</p>");
                 }
                 console.log(data);
                 console.log('url:("https://image.tmdb.org/t/p/w640' + data['poster_path'] + '")');
                 $("#resultOmdb").prepend('<img id="posterMovie" src="' + 'https://image.tmdb.org/t/p/w640' + data['poster_path'] + '">');
-                $("#resultOmdb img").css("float", "left");
-                $("#movieDetails").css("float", "left");
-                $("#movieDetails").css("marginLeft", "7%");
                 $("#movieDetails").css("marginTop", "7%");
                 $("#originalTitle").css("margin-top", "-10%");
                 $("#resultDiv").css('background', 'none');
