cbaec7091e3a1ffed81db3a45c55ced2cb322728
Agnes Broomé Clason <agnesbroomeclason@agnes-air.clients.internal.mah.se>
2017-03-14 11:26:07 +0100

    Update local

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -40,8 +40,12 @@ $("#random").on("click", function getMovie() {
                 if (data.title != data.original_title){
                     $("#movieDetails").append("<h3>" + data.title + "</h3>" + "<h3 id='originalTitle'> <small>" +  data.original_title + "</small> </h3>" + "<h4>" + date.getFullYear() + "</h4>" + "<h4>" + data.production_countries[0]["name"] + "</h4>" + "<p>" + data.overview + "</p>");}
                 else {
-                    $("#movieDetails").append("<h3>" + data.original_title + "</h3>" + "<h4>" + date.getFullYear() + "</h4>" + "<h4>" + data.production_countries[0]["name"] + "</h4>" + "<p>" + data.overview + "</p>");
+
+                    $("#movieDetails").append("<h3 id='originalTitle'>" + data.original_title + "</h3>" + "<h4>" + date.getFullYear() + "</h4>" + "<h4>" + data.production_countries[0]["name"] + "</h4>" + "<p>" + data.overview + "</p>" );
+
                 }
+                $("#save").html('');
+                $("#save").append("<a class='saveB btn btn-default btn-lg' target='_blank'><span class='network-name'>SAVE </span> </a>");
                 console.log(data);
                 //console.log('url:("https://image.tmdb.org/t/p/w640' + data['poster_path'] + '")');
                 $("#resultOmdb").prepend('<img id="posterMovie" src="' + 'https://image.tmdb.org/t/p/w640' + data['poster_path'] + '">');
